796c11281700a0cb81c9b6d8594439103df614e9
kahramanakyil <kahramanakyil@gmail.com>
2024-12-21 14:28:01 +0300

    Update http-common.js

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: process.env.BACKEND_URL/api,
+  baseURL: process.env.BACKEND_URL,
   headers: {
     "Content-type": "application/json"
   }
