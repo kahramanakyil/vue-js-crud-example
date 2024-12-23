10de7c0c2791c38b3a27065fb7e8fb6e2af94b9a
kahramanakyil <kahramanakyil@gmail.com>
2024-12-23 12:48:22 +0300

    Update http-common.js

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
   
 export default axios.create({
-  baseURL: "http://cae-test-backend:35555/api",
+  baseURL: "http://backend.default.svc.cluster.local:9090/api",
   headers: {
     "Content-type": "application/json"
   }
