c5f285fbdd7f93ec5affe2a7399b6fac35680ff0
kahramanakyil <kahramanakyil@gmail.com>
2024-12-24 18:34:18 +0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   devServer: {
-    port: 8081
+    proxy: {
+      '^/api': {
+        target: process.env.VUE_APP_URL,
+        ws: true,
+        changeOrigin: true
+      }
+    }
   }
 }
